67d2c5871b403480c383a865e43f6e330f248db7
mhmadamrii <interceptorghost4@gmail.com>
2025-11-25 17:39:08 +0700

    initial commit

---
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "@test-case/api/routers/index";
+import { createContext } from "@test-case/api/context";
+import { NextRequest } from "next/server";
+
+function handler(req: NextRequest) {
+	return fetchRequestHandler({
+		endpoint: "/api/trpc",
+		req,
+		router: appRouter,
+		createContext: () => createContext(req),
+	});
+}
+export { handler as GET, handler as POST };
